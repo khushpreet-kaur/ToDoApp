060d93cd2787aa444b4410f802229adef0c61005
khushpreet-kaur <khushpreetk363@gmail.com>
2015-08-13 15:04:35 +0530

    Add clear button to remove Done tasks   - DONE button strikes on the done tasks.   - CLEAR button provides the functionality to remove the done task.   - TODO: Need to refactor the code.

--- todo.js
+++ todo.js
@@ -2,21 +2,41 @@ var seq = 0;
 window.WhoAmI = "sdjhskjfnsd"
 function submit() {
   var nameInfo = document.getElementById("name").value;
-  console.log(nameInfo)
-    seq = seq+1;
-  var div = document.createElement('div');
-  div.className = 'new-rect';
-  div.id = "divId" + seq;
-  var b = document.getElementById('disp').appendChild(div);
-  b.innerHTML=nameInfo;
-  var button = document.createElement('button');
-  button.className = "btn btn-default btn-sm";
-  button.innerHTML = "Done";
-  button.onclick = doneClick;
-  div.appendChild(button);
+  var empty = "";
+  document.getElementById("name").value = empty;
+  if (nameInfo == "") {
+    alert("Please enter any task");
+  }
+  else{
+    console.log(nameInfo)
+      seq = seq+1;
+    var div = document.createElement('div');
+    div.className = 'new-rect';
+    div.id = "divId" + seq;
+    var b = document.getElementById('disp').appendChild(div);
+    b.innerHTML=seq + ". " +  nameInfo;
+    var button = document.createElement('button');
+    var button1 = document.createElement('button');
+    button.className = "btn btn-default btn-xs";
+    button1.className = "btn btn-default btn-xs";
+    button1.id = "button1"
+    button.innerHTML = "Done";
+    button1.innerHTML = "Clear Done";
+    button.onclick = doneClick;
+    button1.onclick = clearClick;
+    div.appendChild(button);
+    div.appendChild(button1);
+  }
 }
 function doneClick(){
   var currentDiv = this.parentNode;
   currentDiv.style.textDecoration= 'line-through';
   console.log(this.parentNode);
+}
+
+function clearClick() {
+  console.log("Clear called");
+  var currentDiv = this.parentNode;
+  currentDiv.style.display = 'none';
+  //currentDiv.remove();
 }
